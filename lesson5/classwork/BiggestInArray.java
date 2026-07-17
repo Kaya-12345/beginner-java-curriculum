783ce3c948d622de0ad8e52368277919a3251a1e
Kaya-12345 <purpledog13131@gmail.com>
2026-07-18 05:44:07 +0900

    Update BiggestInArray.java

--- lesson5/classwork/BiggestInArray.java
+++ lesson5/classwork/BiggestInArray.java
@@ -3,7 +3,7 @@ public class BiggestInArray{
 		int[] nums = {1, 2, 3, 20, 4};
 		
 		int biggest = nums[0]; // Start by assuming the first item is the biggest
-		for(int i = 0; i < biggest.length; i++){ // Go through each item in the array
+		for(int i = 0; i < nums.length; i++){ // Go through each item in the array
 			if(nums[i] > biggest){ // If we find something bigger, update our guess
 				biggest = nums[i];
 			}
