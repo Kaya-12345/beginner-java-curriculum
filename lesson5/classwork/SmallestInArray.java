33c4fe465506907aed07e80d425fc38106cd62e8
Kaya-12345 <purpledog13131@gmail.com>
2026-07-18 05:44:26 +0900

    Update SmallestInArray.java

--- lesson5/classwork/SmallestInArray.java
+++ lesson5/classwork/SmallestInArray.java
@@ -3,7 +3,7 @@ public class SmallestInArray{
 		int[] nums = {1, 2, 3, 20, 4};
 		
 		int smallest = nums[0]; // Start by assuming the first item is the smallest
-		for(int i = 0; i < smallest.length; i++){ // Go through each item in the array
+		for(int i = 0; i < nums.length; i++){ // Go through each item in the array
 			if(nums[i] < smallest){ // If we find something smaller, update our guess
 				smallest = nums[i];
 			}
