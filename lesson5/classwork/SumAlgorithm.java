0ebc5ed6073d017a68bdb6d191103bf92adacf97
Kaya-12345 <purpledog13131@gmail.com>
2026-07-18 05:24:13 +0900

    Update SumAlgorithm.java

---
+++ lesson5/classwork/SumAlgorithm.java
@@ -0,0 +1,11 @@
+public class SumAlgorithm{
+	public static void main(String[] args){
+		int[] number = {5, -8, 3, 6, 7};
+		
+		int total = 0;
+		for(int i = 0; i < numbers.length; i++){
+			total = total + numbers[i];
+		}
+		System.out.println("The sum is: " + total);
+	}
+}
